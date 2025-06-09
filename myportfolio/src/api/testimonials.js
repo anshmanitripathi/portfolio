daf0a8f0867a8a5df361673f59a425f08ee1f9e3
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2025-06-09 20:07:32 +0530

    fix some bugs

--- myportfolio/src/api/testimonials.js
+++ myportfolio/src/api/testimonials.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL || 'https://portfolio-backend-al1i.onrender.com/api/';
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 'https://portfolio-backend-al1i.onrender.com/api';
+
 
 // Fetch all testimonials
 export const getTestimonials = async () => {
